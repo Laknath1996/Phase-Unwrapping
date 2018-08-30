fcfe41ac5c5e80bee635b28af823da889b9511b8
Ashwin de Silva <ashwin@Ashwins-MacBook-Pro-2.local>
2018-08-31 09:46:37 +1000

    Update

--- sample_predict.py
+++ sample_predict.py
@@ -12,6 +12,8 @@ import argparse
 parser = argparse.ArgumentParser(description='Predict sample using the trained model')
 parser.add_argument('--pwrap_path', type=str,
                     help='Specify the path of the pwrap dataset')
+parser.add_argument('--orig_path', type=str,
+                    help='Specify the path of the orig dataset')
 parser.add_argument('--weight_path', type=str,
                     help='Specify the path for the weights to be saved')
 args = parser.parse_args()
@@ -21,11 +23,12 @@ IMG_HEIGHT = 512
 IMG_WIDTH = 512
 WEIGHT_DIR = args.weight_path  # '/home/563/ls1729/gdata/phase_unwrapping/weights/PU_unet_002.hdf5'
 PWRAP_DATASET_PATH = args.pwrap_path  # '/home/563/ls1729/gdata/phase_unwrapping/dataset/coco/pwrap/pwrap_dataset.hdf5'
+ORIG_DATASET_PATH = args.orig_path
 
 # load the datasets
 print('Loading the Datasets...')
 pwrap_train_images, pwrap_val_images, pwrap_test_images = load_h5py_datasets(PWRAP_DATASET_PATH)
-
+orig_train_images, orig_val_images, orig_test_images = load_h5py_datasets(ORIG_DATASET_PATH)
 
 print('Making predictions on train, val and test sets...')
 
@@ -39,40 +42,46 @@ ix = random.randint(0, len(preds_test)-1)
 
 im_pwrap_train = np.reshape(pwrap_train_images[ix], (IMG_HEIGHT, IMG_WIDTH))
 im_res_train = np.reshape(preds_train[ix], (IMG_HEIGHT, IMG_WIDTH))
+im_orig_train = np.reshape(orig_train_images[ix], (IMG_HEIGHT, IMG_WIDTH))
 
 im_pwrap_val = np.reshape(pwrap_val_images[ix], (IMG_HEIGHT, IMG_WIDTH))
 im_res_val = np.reshape(preds_val[ix], (IMG_HEIGHT, IMG_WIDTH))
+im_orig_val = np.reshape(orig_val_images[ix], (IMG_HEIGHT, IMG_WIDTH))
 
 im_pwrap_test = np.reshape(pwrap_test_images[ix], (IMG_HEIGHT, IMG_WIDTH))
 im_res_test = np.reshape(preds_test[ix], (IMG_HEIGHT, IMG_WIDTH))
+im_orig_test = np.reshape(orig_test_images[ix], (IMG_HEIGHT, IMG_WIDTH))
 
 print('Saving Results...')
 
-fig, ax = plt.subplots(1, 2, sharex=True, sharey=True)
-ax1, ax2 = ax.ravel()
+fig, ax = plt.subplots(1, 3, sharex=True, sharey=True)
+ax1, ax2, ax3 = ax.ravel()
 fig.colorbar(ax1.imshow(im_pwrap_train, cmap='gray'), ax=ax1)
 ax1.set_title('Wrapped Image')
-fig.colorbar(ax2.imshow(im_res_train, cmap='gray'),
-             ax=ax2)
+fig.colorbar(ax2.imshow(im_res_train, cmap='gray'), ax=ax2)
 ax2.set_title('Unwrapped Image')
+fig.colorbar(ax3.imshow(im_orig_train, cmap='gray'), ax=ax3)
+ax3.set_title('Original Image')
 plt.savefig('/home/563/ls1729/gdata/phase_unwrapping/samples/train_sample.jpg')
 
-fig, ax = plt.subplots(1, 2, sharex=True, sharey=True)
-ax1, ax2 = ax.ravel()
+fig, ax = plt.subplots(1, 3, sharex=True, sharey=True)
+ax1, ax2, ax3 = ax.ravel()
 fig.colorbar(ax1.imshow(im_pwrap_val, cmap='gray'), ax=ax1)
 ax1.set_title('Wrapped Image')
-fig.colorbar(ax2.imshow(im_res_val, cmap='gray'),
-             ax=ax2)
+fig.colorbar(ax2.imshow(im_res_val, cmap='gray'), ax=ax2)
 ax2.set_title('Unwrapped Image')
+fig.colorbar(ax3.imshow(im_orig_val, cmap='gray'), ax=ax3)
+ax2.set_title('Original Image')
 plt.savefig('/home/563/ls1729/gdata/phase_unwrapping/samples/val_sample.jpg')
 
-fig, ax = plt.subplots(1, 2, sharex=True, sharey=True)
-ax1, ax2 = ax.ravel()
+fig, ax = plt.subplots(1, 3, sharex=True, sharey=True)
+ax1, ax2, ax3 = ax.ravel()
 fig.colorbar(ax1.imshow(im_pwrap_test, cmap='gray'), ax=ax1)
 ax1.set_title('Wrapped Image')
-fig.colorbar(ax2.imshow(im_res_test, cmap='gray'),
-             ax=ax2)
+fig.colorbar(ax2.imshow(im_res_test, cmap='gray'), ax=ax2)
 ax2.set_title('Unwrapped Image')
+fig.colorbar(ax3.imshow(im_orig_test, cmap='gray'), ax=ax3)
+ax2.set_title('Original Image')
 plt.savefig('/home/563/ls1729/gdata/phase_unwrapping/samples/test_sample.jpg')
 
 print('Complete..!')
