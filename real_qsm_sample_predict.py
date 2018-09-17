18355b66028164ecba8a5d040fcac05f97f4baea
Ashwin de Silva <ashwin@Ashwins-MBP-2.gateway>
2018-09-18 06:55:24 +1000

    Update

--- real_qsm_sample_predict.py
+++ real_qsm_sample_predict.py
@@ -46,7 +46,7 @@ print('Making predictions on real qsm data...')
 pwrap = pwrap_images[70:91]
 model = load_model(WEIGHT_DIR)
 pred_images = model.predict(pwrap_images[70:91], verbose=1)
-pred_images = np.reshape(pred_images, (np.size(pwrap_images, 1), np.size(pwrap_images, 2), np.size(pwrap, 0)))
+# pred_images = np.reshape(pred_images, (np.size(pwrap_images, 1), np.size(pwrap_images, 2), np.size(pwrap, 0)))
 
 # compute the error
 
