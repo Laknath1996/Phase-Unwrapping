313120fa8ce1a8d6cfd42fafae2b0d276e719466
Ashwin de Silva <ashwin@Ashwins-MacBook-Pro-2.local>
2018-09-19 11:30:35 +1000

    Update

--- pick_nat_images.py
+++ pick_nat_images.py
@@ -10,7 +10,7 @@ from shutil import copyfile
 # define useful params
 IMGS_PER_CAT = 10
 DATA_PATH = '/home/563/ls1729/gdata/phase_unwrapping/dataset/coco/val2017/'
-SAVE_PATH = '/home/563/ls1729/gdata/phase_unwrapping/dataset/coco/orig/images_2/'
+SAVE_PATH = '/home/563/ls1729/gdata/phase_unwrapping/dataset/coco/orig/images_2'
 
 # get the annotation file path
 annFile = '/home/563/ls1729/gdata/phase_unwrapping/dataset/coco/annotations/instances_val2017.json'
