38b2da1baf59bc742c48506362eb28d9eb0e69d0
Ashwin de Silva <ashwin@Ashwins-MacBook-Pro-2.local>
2018-09-19 11:15:47 +1000

    Update

--- pick_nat_images.py
+++ pick_nat_images.py
@@ -10,17 +10,17 @@ from shutil import copyfile
 # define useful params
 IMGS_PER_CAT = 10
 DATA_PATH = '/home/563/ls1729/gdata/phase_unwrapping/dataset/coco/val2017/'
-SAVE_PATH = '/home/563/ls1729/gdata/phase_unwrapping/dataset/coco/orig/images_2'
+SAVE_PATH = '/home/563/ls1729/gdata/phase_unwrapping/dataset/coco/orig/images_2/'
 
 # get the annotation file path
-annFile='/home/563/ls1729/gdata/phase_unwrapping/dataset/coco/annotations/instances_val2017.json'
+annFile = '/home/563/ls1729/gdata/phase_unwrapping/dataset/coco/annotations/instances_val2017.json'
 
 # initialize COCO api for instance annotations
 coco = COCO(annFile)
 
 # load the categories
 cats = coco.loadCats(coco.getCatIds())
-cat_names = ['cat','dog','cake','apple','phone']
+cat_names = ['cat', 'dog', 'cake', 'apple', 'phone']
 
 # pick images from each category
 for i in range(len(cat_names)):
